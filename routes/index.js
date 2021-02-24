17c37272595deb9fc99e62628403f819fd910d43
Veysel Usta <veyselusta7@gmail.com>
2021-02-25 01:04:33 +0300

    Define post route to create new user

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
 const router = require('express').Router()
+const User = require('../models/user')
 
 router.get('/', (req, res) => {
+  res.send('ok')
+})
+
+router.post('/create', async (req, res) => {
+  const { name } = req.body
+
+  await User.create({ name })
   res.send('ok')
 })
 
