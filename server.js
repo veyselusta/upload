89269da73be0bb6798236720bc7d5c2e3b832e3f
Veysel Usta <veyselusta7@gmail.com>
2021-02-25 01:03:00 +0300

    Install body-parser

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const indexRouter = require('./routes/index')
+//connect mongoDB database
 require('./mongo-connection')
 
 const app = express()
+
+app.use(bodyParser.json())
 
 app.use('/', indexRouter)
 
